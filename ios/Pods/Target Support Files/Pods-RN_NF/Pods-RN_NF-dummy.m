684a64cd7f2c273d8c5d672374d49a749aaadc5a
wanzz8 <white.wanj@gmail.com>
2018-10-09 20:23:46 +0800

    1.删除不成功的配置文件

--- ios/Pods/Target Support Files/Pods-RN_NF/Pods-RN_NF-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RN_NF : NSObject
-@end
-@implementation PodsDummy_Pods_RN_NF
-@end
--- ios/Pods/Target Support Files/Pods-RN_NFTests/Pods-RN_NFTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RN_NFTests : NSObject
-@end
-@implementation PodsDummy_Pods_RN_NFTests
-@end
